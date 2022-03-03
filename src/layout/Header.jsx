1a55f195ffa91216945d502e92c673eced2c02a3
Maria Krayevskaya <43548440+riaskay27@users.noreply.github.com>
2022-03-03 17:23:09 +0200

    first commit

---
+++ src/layout/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+    return <nav className="#4db6ac teal lighten-2">
+    <div className="nav-wrapper">
+      <a href="#" className="brand-logo">React Movies</a>
+      <ul id="nav-mobile" className="right hide-on-med-and-down">
+        <li><a href="!#">Repo</a></li></ul>
+    </div>
+  </nav>
+}
+
+export {Header}
